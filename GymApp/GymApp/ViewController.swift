d05c7106c7ee17b2486f4fe1e728593677e4c362
midaaaa <fildima8423@gmail.com>
2025-10-29 20:00:10 +0300

    Empty Xcode project creation and README.md enrichment

---
+++ GymApp/GymApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GymApp
+//
+//  Created by Дмитрий Филимонов on 29.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
